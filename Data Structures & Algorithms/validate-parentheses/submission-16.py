49a586dc141564322157fd7d6f24d98b71ff0aa2
OctavioValdiviaMendoza <octavio.valdiviamendoza@sjsu.edu>
2026-09-14 18:46:04 -0700

    Add: validate-parentheses - submission-16

---
+++ Data Structures & Algorithms/validate-parentheses/submission-16.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        '''
+        return False if not valid
+        Possible False Cases:
+        string size is not even 
+        brackets don't match 
+        '''
+        if not s:
+            return False
+        n = len(s)
+        if n % 2 == 1:
+            return False
+        
+        stack = []
+        for par in s:
+            if par == '[':
+                stack.append(']')
+            elif par == '(':
+                stack.append(')')
+            elif par == '{':
+                stack.append('}')
+            else:
+                if len(stack) == 0:
+                    return False
+                comp_par = stack.pop()
+                if comp_par != par:
+                    return False
+        if len(stack) != 0:
+            return False
+        return True
+
+
+
+
+        
